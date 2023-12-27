d5c7a778abe4685e15eb9db1c5aab362f4e1609e
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-27 16:37:18 +0530

    added hero section edites

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,8 +7,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 const images = [
   "/herosection/1.svg",
+  
   "/herosection/2.svg",
+
   "/herosection/3.svg",
+
   "/herosection/4.svg",
 ];
 
@@ -37,7 +40,7 @@ const Hero = () => {
 
   return (
     <>
-        <section id="home" className=" pb-20  md:pt-10 xl:pb-25 xl:pt-1 relative" >
+    <section id="home" className=" pb-20  md:pt-10 xl:pb-25 xl:pt-1 relative" >
   <div className="pt-7 sm:pt-16 lg:pt-20 mx-auto w-full h-full">
     <div className=" md:block scale-95 rounded-lg relative">
       <Slider {...settings} className="">
@@ -112,6 +115,9 @@ const Hero = () => {
 
 
 
+
+
+
       {/* <section>   <Image
                   src="/images/shape/shape-01.svg"
                   alt="shape"
@@ -124,81 +130,104 @@ const Hero = () => {
 
 
 
+{/* 
+<section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <section className="flex items-center justify-center">
-      
-    </section>
-    <section className="p-4 md:p-8 lg:p-12 flex flex-col items-center justify-center" style={{ marginTop: "-14rem" }}>
-  <div className="flex flex-col md:flex-row justify-center items-center">
-    {/* First Card */}
-    <div className="max-w-md md:max-w-lg lg:max-w-xl mx-4 mb-4 md:mb-0 overflow-hidden shadow-lg bg-white dark:bg-btndark rounded-full p-4 flex transition-transform transform hover:translate-y-[-10px] hover:scale-105">
-      {/* Circular Image */}
-      <div className="w-16 h-16 overflow-hidden rounded-full mr-4">
-        <img
-          className="w-full h-full object-cover"
-          src="https://placekitten.com/200/200"
-          alt="Card 1"
-        />
+<div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 p-10 xl:-mt-30">
+  <div className="min-w-0 rounded-full overflow-hidden shadow-md bg-white hover:bg-gradient-to-br hover:from-cyan-400 hover:via-blue-400 hover:to-blue-500 transition-all duration-300 relative group border-2 border-gray-100 dark:bg-gray-800">
+    <div className="p-4 flex items-center">
+      <div
+        className="p-3 rounded-full text-orange-500 dark:text-orange-100 bg-orange-100 dark:bg-orange-500 mr-4"
+      >
+        <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
+          <path
+            d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+          ></path>
+        </svg>
       </div>
-
-      {/* Card Content */}
-      <div className="flex flex-col">
-        <h2 className="text-xl font-bold text-gray-800 dark:text-white">2</h2>
-        <p className="text-gray-600 dark:text-gray-400 mt-2 flex-grow">Succesfully running engineer scoops</p>
-      </div>
-    </div>
-
-    {/* Second Card */}
-    <div className="max-w-md md:max-w-lg lg:max-w-xl mx-4 mb-4 md:mb-0 overflow-hidden shadow-lg bg-white dark:bg-btndark rounded-full p-4 flex transition-transform transform hover:translate-y-[-10px] hover:scale-105">
-      {/* Circular Image */}
-      <div className="w-16 h-16 overflow-hidden rounded-full mr-4">
-        <img
-          className="w-full h-full object-cover"
-          src="https://placekitten.com/200/201"
-          alt="Card 2"
-        />
-      </div>
-
-      {/* Card Content */}
-      <div className="flex flex-col">
-        <h2 className="text-xl font-bold text-gray-800 dark:text-white">5</h2>
-        <p className="text-gray-600 dark:text-gray-400 mt-2 flex-grow">Succesfully running engineer scoops</p>
-      </div>
-    </div>
-
-    {/* Third Card */}
-    <div className="max-w-md md:max-w-lg lg:max-w-xl mx-4 overflow-hidden shadow-lg bg-white dark:bg-btndark rounded-full p-4 flex transition-transform transform hover:translate-y-[-10px] hover:scale-105">
-      {/* Circular Image */}
-      <div className="w-16 h-16 overflow-hidden rounded-full mr-4">
-        <img
-          className="w-full h-full object-cover"
-          src="https://placekitten.com/201/200"
-          alt="Card 3"
-        />
-      </div>
-
-      {/* Card Content */}
-      <div className="flex flex-col">
-        <h2 className="text-xl font-bold text-gray-800 dark:text-white">2</h2>
-        <p className="text-gray-600 dark:text-gray-400 mt-2 flex-grow">Succesfully running engineer scoops</p>
+      <div>
+        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          Total clients
+        </p>
+        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+        900
+        </p>
       </div>
     </div>
   </div>
-</section>
+ <div className="min-w-0 rounded-full overflow-hidden shadow-md bg-white hover:bg-gradient-to-br hover:from-cyan-400 hover:via-blue-400 hover:to-blue-500 transition-all duration-300 relative group border-2 border-gray-100 dark:bg-gray-800">
+    <div className="p-4 flex items-center">
+      <div
+        className="p-3 rounded-full text-green-500 dark:text-green-100 bg-green-100 dark:bg-green-500 mr-4"
+      >
+        <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
+          <path
+            fill-rule="evenodd"
+            d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          Team
+        </p>
+        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+          100
+        </p>
+      </div>
+    </div>
+   </div>
+  <div className="min-w-0 rounded-full overflow-hidden shadow-md bg-white hover:bg-gradient-to-br hover:from-cyan-400 hover:via-blue-400 hover:to-blue-500 transition-all duration-300 relative group border-2 border-gray-100 dark:bg-gray-800">
+    <div className="p-4 flex items-center">
+      <div
+        className="p-3 rounded-full text-blue-500 dark:text-blue-100 bg-blue-100 dark:bg-blue-500 mr-4"
+      >
+        <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
+          <path
+            d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          Projects Completed
+        </p>
+        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+          376
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="min-w-0 rounded-full overflow-hidden shadow-md bg-white hover:bg-gradient-to-br hover:from-cyan-400 hover:via-blue-400 hover:to-blue-500 transition-all duration-300 relative group border-2 border-gray-100 dark:bg-gray-800">
+    <div className="p-4 flex items-center">
+      <div
+        className="p-3 rounded-full text-teal-500 dark:text-teal-100 bg-teal-100 dark:bg-teal-500 mr-4"
+      >
+        <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
+          <path
+            fill-rule="evenodd"
+            d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          Industry Expertise
+        </p>
+        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">5+ years</p>
+      </div>
+    </div>
+  </div>
+</div>
+</section> */}
+
+
+
+
+
+
 
     </>
   );
