c714c0a0917a52de2ca8d32232dc390bf74ccc91
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-27 16:23:27 +0530

    hero section changes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -37,9 +37,9 @@ const Hero = () => {
 
   return (
     <>
-       <section id="home" className="overflow-hidden pb-20  md:pt-10 xl:pb-25 xl:pt-1 relative" >
+        <section id="home" className=" pb-20  md:pt-10 xl:pb-25 xl:pt-1 relative" >
   <div className="pt-7 sm:pt-16 lg:pt-20 mx-auto w-full h-full">
-    <div className="hidden md:block scale-95 rounded-lg relative">
+    <div className=" md:block scale-95 rounded-lg relative">
       <Slider {...settings} className="">
         {images.map((image, index) => {
           return (
@@ -51,23 +51,43 @@ const Hero = () => {
                 className="rounded-lg cursor-pointer"
               />
               <div className="hero-text-container absolute left-0 text-left pl-6 top-1/2 transform -translate-y-1/2">
-              <h4 className="mb-1 heading-small text-2xl font-medium text-black dark:text-white">
+              <h4 className="mb-6 heading-small text-xl font-medium text-white dark:text-white" >
                  Welcome to
               </h4>
-              <h1 className="mb-2 pr-16 heading-text font-bold text-blue-600 dark:text-white xl:text-hero hover:translate-y-[-18px]">
-              Atoms Group
+              <h1 className=" text-4xl font-bold pr-16 heading-text  text-white dark:text-white xl:text-hero hover:translate-y-[-18px]" style={{ marginBottom:"-15px" }}>
+              Atoms Education
               </h1>
-              <div className="text-2xl">   
-              <p className="w-1/2">
+              <div className="text-xl">   
+              <p className="mt-8 w-1/2 text-white font-regular">
               Atoms Education is committed to providing affordable and impressive Education+ services for people from all social backgrounds.
               </p>
               </div>
               <div className="mt-8">
-                             
+                {/* <form onSubmit={handleSubmit}> */}
+                  {/* <div className="flex flex-wrap gap-5"> */}
+                    {/* <input
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      type="text"
+                      placeholder="Enter your email address"
+                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
+                    /> */}
+                    {/* <button
+                      aria-label="get started button"
+                      className="flex rounded-full bg-purple-600 px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-purple-600 dark:hover:bg-blackho hover:translate-y-[-5px]"
+                    >
+                      Our Projects
+                    </button>
+                    <button
+                      aria-label="get started button"
+                      className="flex rounded-full bg-gray-100  px-7.5 py-2.5 text-black  dark:text-white hover:text-white duration-300 ease-in-out hover:bg-purple-600 dark:bg-btndark dark:hover:bg-purple-600 hover:translate-y-[-5px]"
+                    >
+                      Our Team
+                    </button>
+                  </div> */}
+               
 
-                <p className="mt-5 text-black dark:text-white">
-                  go though the site to know about our services...
-                </p>
+               
               </div>
               </div>
             </div>
@@ -76,7 +96,7 @@ const Hero = () => {
       </Slider>
     </div>
     <div className="block md:hidden scale-95 rounded-lg pt-6 relative">
-   
+      
     </div>
   </div>
 </section>
